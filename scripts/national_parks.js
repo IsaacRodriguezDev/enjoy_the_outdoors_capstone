02ad01736f8df77205cc0bf3b2e4a8a53a9d3ff0
IsaacRodriguezDev <isaacrodriguez.dev@gmail.com>
2024-05-20 11:38:02 -0500

    Added new options to location dropdown category from a provided array

--- scripts/national_parks.js
+++ scripts/national_parks.js
@@ -1,22 +1,30 @@
-"use strict"
+"use strict";
 
 window.onload = () => {
+  // when page loads hide the table header
+  document.querySelector("#tableContent").style.display = "none";
 
-    /*
-        You can remove the following console.log() lines.
-        They are here to verify that we have access to the data
-        The data script files are located in the scripts/data directory
-    */
+  // called locationDropdownContent function to display the categories on the locationdropdown as soon as pageloads
+  locationDropdownContent();
 
-    //log the locationsArray to the console (scripts/data/locationData.js)
-    console.log(locationsArray)
+};
 
-    //log the parkTypesArray to the console (scripts/data/parkTypeData.js)
-    console.log(parkTypesArray)
+// created a function called locationDropdownContent to add every content in the locationsArray into the dropdown for user to select
+function locationDropdownContent() {
+    
+  //   created a variable called defualtOption to display select a park location on the dropdown and appended it to the location dropdown
+  let locationDowndown = document.querySelector("#parkLocationDropdown");
+  let defaultOption = document.createElement("option");
+  defaultOption.textContent = "--Select a Park Location--";
+  defaultOption.value = "";
+  locationDowndown.appendChild(defaultOption);
 
-    //log the nationalParksArray to the console (scripts/data/nationalParkData.js)
-    console.log(nationalParksArray)
-
-
-
+  //  used forEach to loop through locationsArray to display all locationArray as categories
+  locationsArray.forEach((location) => {
+    let stateOption = document.createElement("option");
+    stateOption.textContent = location;
+    stateOption.value = location;
+    locationDowndown.appendChild(stateOption);
+  });
 }
+
